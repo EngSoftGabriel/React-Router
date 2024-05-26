ffd10c6b514c5f40f684596d1e3bc3149c044989
gabriels <gabrielg.sw@hotmail.com>
2024-05-25 23:13:38 -0300

    curso react

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,10 @@
+import './Content.css'
+import React from 'react'
+
+const Content = props => (
+    <aside className='Content'>
+        Componente Content
+    </aside>
+)
+
+export default Content
---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Home.css'
+
+const Home = props => (
+    <aside className='Home'>
+        Componente Home
+    </aside>
+)
+
+export default Home
